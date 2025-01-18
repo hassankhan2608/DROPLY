c29f6f3bdbb6b2fedcf58520000332e97bf6bc9e
Mohd Hassan Khan <149296407+hassankhan2608@users.noreply.github.com>
2025-01-18 22:02:49 +0530

    experiment

---
+++ src/lib/customDictionaries.js
@@ -0,0 +1,73 @@
+// Mystical Creatures
+export const mysticalCreatures = [
+  'Dragon', 'Phoenix', 'Unicorn', 'Griffin', 'Mermaid', 'Centaur', 'Hydra', 'Chimera', 'Basilisk', 'Kraken',
+  'Sphinx', 'Minotaur', 'Cyclops', 'Gorgon', 'Harpy', 'Leviathan', 'Manticore', 'Nymph', 'Satyr', 'Sylph',
+  'Valkyrie', 'Werewolf', 'Yeti', 'Zombie', 'Banshee', 'Djinn', 'Faun', 'Ghoul', 'Imp', 'Kelpie',
+  'Lamia', 'Naga', 'Ogre', 'Pixie', 'Rakshasa', 'Selkie', 'Sprite', 'Troll', 'Wendigo', 'Wyvern',
+  'Zaratan', 'Aswang', 'Chupacabra', 'Dybbuk', 'Elf', 'Fomorian', 'Golem', 'Hippogriff', 'Jotunn', 'Kitsune'
+];
+
+// Superheroes
+export const superheroes = [
+  'Superman', 'Batman', 'WonderWoman', 'SpiderMan', 'IronMan', 'Thor', 'Hulk', 'Flash', 'Aquaman', 'Cyborg',
+  'Daredevil', 'Hawkeye', 'BlackWidow', 'GreenLantern', 'Wolverine', 'Cyclops', 'Storm', 'Rogue', 'Gambit', 'Beast',
+  'Nightcrawler', 'Colossus', 'Iceman', 'JeanGrey', 'ProfessorX', 'Magneto', 'Deadpool', 'DoctorStrange', 'BlackPanther', 'AntMan',
+  'Wasp', 'ScarletWitch', 'Vision', 'Quicksilver', 'StarLord', 'Gamora', 'Drax', 'Rocket', 'Groot', 'Nebula',
+  'Mantis', 'WarMachine', 'Falcon', 'WinterSoldier', 'Shazam', 'GreenArrow', 'BlueBeetle', 'Zatanna', 'MartianManhunter', 'PlasticMan'
+];
+
+// Gods of War
+export const godsOfWar = [
+  'Ares', 'Mars', 'Anhur', 'Ankt', 'Baal', 'Huitzilopochtli', 'Kartikeya', 'Maahes', 'Montu', 'Ogun',
+  'Tyr', 'Wepwawet', 'Zababa', 'Indra', 'Sekhmet', 'Bellona', 'Andraste', 'Camulus', 'Morrigan', 'Neith',
+  'Nike', 'Oya', 'Pele', 'Tezcatlipoca', 'XipeTotec', 'ZaoJun', 'Chiyou', 'GuanYu', 'Hachiman', 'Takemikazuchi',
+  'Susanoo', 'Tlaloc', 'Mixcoatl', 'Itzamna', 'AhPuch', 'Kukulkan', 'Viracocha', 'Inti', 'Pachacamac', 'Supay',
+  'Vahagn', 'Verethragna', 'Mitra', 'Perun', 'Svantevit', 'Triglav', 'Jarovit', 'Rudra', 'Skanda', 'Murugan'
+];
+
+// Anime Characters
+export const animeCharacters = [
+  'Goku', 'Naruto', 'Luffy', 'Ichigo', 'Saitama', 'Eren', 'Light', 'Edward', 'Alphonse', 'Natsu',
+  'Lucy', 'Sakura', 'Kakashi', 'Levi', 'Mikasa', 'Gon', 'Killua', 'Yusuke', 'Hiei', 'Inuyasha',
+  'Kagome', 'Spike', 'Vash', 'Shinji', 'Asuka', 'Rei', 'Saber', 'Kirito', 'Asuna', 'Rem',
+  'Emilia', 'Tanjiro', 'Nezuko', 'Zenitsu', 'Inosuke', 'Asta', 'Yuno', 'Noelle', 'Meliodas', 'Elizabeth',
+  'Deku', 'Bakugo', 'Todoroki', 'AllMight', 'Erza', 'Gray', 'Juvia', 'Lelouch', 'C.C.', 'Kallen'
+];
+
+// Villains
+export const villains = [
+  'Joker', 'LexLuthor', 'Thanos', 'GreenGoblin', 'Magneto', 'Loki', 'DoctorDoom', 'RedSkull', 'Venom', 'Ultron',
+  'Darkseid', 'Kingpin', 'Ra’sAlGhul', 'Bane', 'Catwoman', 'HarleyQuinn', 'Deathstroke', 'BlackAdam', 'Sinestro', 'ReverseFlash',
+  'Brainiac', 'Scarecrow', 'Penguin', 'Riddler', 'TwoFace', 'Hela', 'Killmonger', 'Mysterio', 'Sandman', 'Electro',
+  'Vulture', 'DoctorOctopus', 'Dormammu', 'Mandarin', 'Apocalypse', 'Mystique', 'Sabretooth', 'Juggernaut', 'OmegaRed', 'MrSinister',
+  'Mephisto', 'Kang', 'Taskmaster', 'BaronZemo', 'Abomination', 'Enchantress', 'BlackManta', 'Deadshot', 'CaptainCold', 'Zoom'
+];
+
+// Marvel Characters
+export const marvelCharacters = [
+  'IronMan', 'CaptainAmerica', 'Thor', 'Hulk', 'BlackWidow', 'Hawkeye', 'SpiderMan', 'DoctorStrange', 'BlackPanther', 'CaptainMarvel',
+  'AntMan', 'Wasp', 'ScarletWitch', 'Vision', 'Falcon', 'WinterSoldier', 'WarMachine', 'StarLord', 'Gamora', 'Drax',
+  'Rocket', 'Groot', 'Mantis', 'Nebula', 'Loki', 'Thanos', 'NickFury', 'MariaHill', 'PhilCoulson', 'PeggyCarter',
+  'Shuri', 'Okoye', 'Nakia', 'Wong', 'HappyHogan', 'Korg', 'Valkyrie', 'Heimdall', 'Odin', 'Frigga',
+  'JaneFoster', 'DarcyLewis', 'ErikSelvig', 'PietroMaximoff', 'Yondu', 'Ego', 'AncientOne', 'Kaecilius', 'Mordo', 'Dormammu'
+];
+
+// Quantum Terms
+export const quantumTerms = [
+  'Quark', 'Lepton', 'Boson', 'Photon', 'Gluon', 'Graviton', 'Higgs', 'Neutrino',
+  'Electron', 'Proton', 'Neutron', 'Hadron', 'Meson', 'Baryon', 'Fermion', 'Spin',
+  'Entanglement', 'Superposition', 'Decoherence', 'Tunneling', 'Interference', 'Wavefunction', 'Eigenstate', 'Observable',
+  'Uncertainty', 'Complementarity', 'Duality', 'Nonlocality', 'Teleportation', 'Cryptography', 'Qubit', 'Superconductivity',
+  'Superfluidity', 'Renormalization', 'Annihilation', 'Creation', 'Symmetry', 'Asymmetry', 'Gauge', 'Invariance',
+  'Covariance', 'Lagrangian', 'Hamiltonian', 'PathIntegral', 'Perturbation', 'Quantization', 'Radiation', 'Vacuum'
+];
+
+export const dcCharacters = [
+  'Superman', 'Batman', 'WonderWoman', 'Flash', 'Aquaman', 'Cyborg', 'GreenLantern', 'MartianManhunter',
+  'Shazam', 'GreenArrow', 'Hawkgirl', 'Hawkman', 'Zatanna', 'BlackCanary', 'Atom', 'Firestorm',
+  'RedTornado', 'BlueBeetle', 'BoosterGold', 'Vixen', 'PlasticMan', 'ElongatedMan', 'Stargirl', 'DrFate',
+  'Spectre', 'Constantine', 'SwampThing', 'Etrigan', 'Orion', 'BigBarda', 'MisterMiracle', 'Lobo',
+  'Starfire', 'Raven', 'BeastBoy', 'Nightwing', 'Batgirl', 'Supergirl', 'PowerGirl', 'Huntress',
+  'Katana', 'BlackLightning', 'Static', 'Icon', 'Rocket', 'Terra', 'Jericho', 'CassandraCain',
+  'TimDrake', 'DamianWayne'
+];
